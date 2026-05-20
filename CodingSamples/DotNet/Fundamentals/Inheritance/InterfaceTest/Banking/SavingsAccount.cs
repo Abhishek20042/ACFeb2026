82ca43e3c69f438c4900a7b8e7f157ae77e72caf
nileshshirkemet <43514175+nileshshirkemet@users.noreply.github.com>
2026-05-20 11:38:27 +0530

    Add files via upload

---
+++ CodingSamples/DotNet/Fundamentals/Inheritance/InterfaceTest/Banking/SavingsAccount.cs
@@ -0,0 +1,30 @@
+namespace Banking;
+
+sealed class SavingsAccount : Account, IProfitable
+{
+    private const decimal MinBal = 5000;
+
+    internal SavingsAccount()
+    {
+        Balance = MinBal;
+    }
+
+    public override void Deposit(decimal amount)
+    {
+        Balance += MinBal;
+    }
+
+    public override void Withdraw(decimal amount)
+    {
+        if(Balance - amount < MinBal)
+            throw new InsufficientFundsException();
+        Balance -= amount;
+    }
+
+    public void AddInterest(int months)
+    {
+       decimal rate = Balance < 5 * MinBal ? 3.5m : 4.0m;
+       decimal interest = Balance * months * rate / 1200;
+       Balance += interest;
+    }
+}
