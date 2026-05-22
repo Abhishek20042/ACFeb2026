706db583d46cf382f059cbadd67e4e485fb32671
nileshshirkemet <43514175+nileshshirkemet@users.noreply.github.com>
2026-05-22 11:09:57 +0530

    Add files via upload

---
+++ CodingSamples/DotNet/Fundamentals/Generic/TypeParamTest/DemoApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace DemoApp;
+
+class Program
+{
+    static void PrintStack(SimpleStack<string> store)
+    {
+        while(!store.Empty())
+            Console.WriteLine(store.Pop());
+    }
+
+    static void PrintStack(SimpleStack<Interval> store)
+    {
+        while(!store.Empty())
+            Console.WriteLine(store.Pop());
+    }
+
+    static void Main(string[] args)
+    {
+        //closed generic type with string for type-parameter
+        SimpleStack<string> a = new SimpleStack<string>();
+        a.Push("Monday");
+        a.Push("Tuesday");
+        a.Push("Wednesday");
+        a.Push("Thursday");
+        a.Push("Friday");
+        //a.Push(12.3);
+        PrintStack(a);
+        SimpleStack<Interval> b = new SimpleStack<Interval>();
+        b.Push(new Interval(4, 31));
+        b.Push(new Interval(7, 12));
+        b.Push(new Interval(6, 53));
+        b.Push(new Interval(5, 24));
+        Console.WriteLine("--------------------------");
+        PrintStack(b);
+    }
+}
---
+++ CodingSamples/DotNet/Fundamentals/Generic/TypeParamTest/DemoApp2/SimpleStack.cs
@@ -0,0 +1,31 @@
+namespace DemoApp;
+
+//defining an open generic type with type-parameter E
+public class SimpleStack<E>
+{
+    //nested class cannot refer non-static members of outer class
+    class Node
+    {
+       internal E value;
+       internal Node below; 
+    }
+
+    private Node top;
+
+    public void Push(E item)
+    {
+        top = new Node { value = item, below = top };
+    }
+
+    public E Pop()
+    {
+        var node = top;
+        top = top.below;
+        return node.value;
+    }
+
+    public bool Empty()
+    {
+        return top is null;
+    }
+}
