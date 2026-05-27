821cc72deaa367bf9f48fd0e809caec08d301b74
nileshshirkemet <43514175+nileshshirkemet@users.noreply.github.com>
2026-05-27 11:57:20 +0530

    Add files via upload

---
+++ CodingSamples/DotNet/Fundamentals/Concurrency/ThreadingTest/DemoApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace DemoApp;
+
+class Program
+{
+    static void HandleJob(int jobId)
+    {
+        Console.WriteLine("Thread<{0}> has accepted job<{1}> for {2}", Thread.CurrentThread.ManagedThreadId, jobId, Activity.Client);
+        Activity.Perform(jobId);
+        Console.WriteLine("Thread<{0}> has finished job<{1}> for {2}", Environment.CurrentManagedThreadId, jobId, Activity.Client);
+    }
+
+    static void Main(string[] args)
+    {
+        int n = args.Length > 0 ? int.Parse(args[0]) : 1;
+        var child = new Thread(() =>
+        {
+            Activity.Client = "Jack";
+            HandleJob(n);
+        });
+        if(n > 10)
+            child.IsBackground = true; //runtime does not wait for a background thread to exit
+        child.Start();
+        Activity.Client = "Jill";
+        HandleJob(2);
+
+    }
+}
---
+++ CodingSamples/DotNet/Fundamentals/Reflection/AttributeTest/DemoApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using Finance;
+
+namespace DemoApp;
+
+using RateFunc = Func<decimal, int, float>;
+//delegate float RateFunc(decimal amount, int period);
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        decimal p = decimal.Parse(args[0]);
+        Type t = Type.GetType($"Finance.{args[1]},BankFinLib", true);
+        decimal pf = t.IsDefined(typeof(FlexibleAttribute)) ? 0.005m * p : 0;
+        object policy = Activator.CreateInstance(t); 
+        MethodInfo scheme = t.GetMethod(args[2]);
+        MaxDurationAttribute md = scheme.GetCustomAttribute<MaxDurationAttribute>();
+        int m = md?.Limit ?? 10; //md != null ? md.Limit : 10;
+        //verify method and locate implementation
+        RateFunc rf = scheme.CreateDelegate<RateFunc>(policy);
+        for(int n = 1; n <= m; ++n)
+        {
+            float r = rf(p, n); //dispatch call
+            decimal emi = LoanHelper.MonthlyInstallment(p, n, r);
+            Console.WriteLine("{0, -6}{1, 16:0.00}", n, emi);
+        }
+        Console.WriteLine("Processing Fees: {0:0.00}", pf);
+    }
+}
