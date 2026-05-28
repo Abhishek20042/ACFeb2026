8c360e23ab8620969bcfbc01ee653af88eb9fa00
nileshshirkemet <43514175+nileshshirkemet@users.noreply.github.com>
2026-05-28 18:04:59 +0530

    Add files via upload

---
+++ CodingSamples/DotNet/Advanced/Interoperation/DllImportTest/DemoApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DemoApp;
+
+class Program
+{
+    [DllImport("dijkstra", EntryPoint = "lcm")]
+    extern static nint LeastMultiple(nint first, nint second);
+
+    static void Main(string[] args)
+    {
+        nint m = nint.Parse(args[0]);
+        nint n = int.Parse(args[1]);
+        Console.WriteLine("L.C.M = {0}", LeastMultiple(m, n));
+    }
+}
---
+++ CodingSamples/DotNet/Advanced/Interoperation/NativeLibTest/DemoApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DemoApp;
+
+class Program
+{
+    delegate double DepreciationInvoker(int life, int after);
+
+    static void Main(string[] args)
+    {
+        double p = double.Parse(args[0]);
+        int l = int.Parse(args[1]);
+        nint lib = NativeLibrary.Load(args[2]);
+        nint fn = NativeLibrary.GetExport(lib, "depreciation");
+        var depr = Marshal.GetDelegateForFunctionPointer<DepreciationInvoker>(fn);
+        for(int n = 1; n < l; ++n)
+        {
+            double d = depr.Invoke(l, n);
+            Console.WriteLine("{0, -4}{1, 16:0.00}", n, p * (1 - d));
+        }
+        NativeLibrary.Free(lib);
+    }
+}
